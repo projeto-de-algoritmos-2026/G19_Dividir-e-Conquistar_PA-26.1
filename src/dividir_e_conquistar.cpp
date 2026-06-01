6ec11bca94304eb8b258ad7b0dd6c5410b4854c3
Artur Mendonça Arruda <arturmendoncaarruda@gmail.com>
2026-06-01 08:24:57 -0300

    refactor: aplicacao de padroes de clean code no motor matematico e inclusao de otimizacao no eixo z

--- src/dividir_e_conquistar.cpp
+++ src/dividir_e_conquistar.cpp
@@ -4,8 +4,6 @@
 #include <cfloat>
 #include <future>
 
-using namespace std;
-
 #include "point.h"
 
 const int LIMIAR_THREAD = 5000;
@@ -19,13 +17,13 @@ bool compararY(const Ponto& a, const Ponto& b) {
 }
 
 double calcularDistancia(const Ponto& p1, const Ponto& p2) {
-    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + 
-                (p1.y - p2.y) * (p1.y - p2.y) + 
-                (p1.z - p2.z) * (p1.z - p2.z));
+    return std::sqrt((p1.x - p2.x) * (p1.x - p2.x) + 
+                     (p1.y - p2.y) * (p1.y - p2.y) + 
+                     (p1.z - p2.z) * (p1.z - p2.z));
 }
 
 // força bruta local para pequenos subconjuntos de sensores (n <= 3)
-double forcaBrutaLocal(const vector<Ponto>& P, int inicio, int fim, Ponto& cp1, Ponto& cp2) {
+double forcaBrutaLocal(const std::vector<Ponto>& P, int inicio, int fim, Ponto& cp1, Ponto& cp2) {
     double min_dist = DBL_MAX;
     for (int i = inicio; i < fim; ++i) {
         for (int j = i + 1; j <= fim; ++j) {
@@ -41,20 +39,22 @@ double forcaBrutaLocal(const vector<Ponto>& P, int inicio, int fim, Ponto& cp1, 
 }
 
 // exporta forca bruta para a main fazer benchmark
-double buscarForcaBruta(const vector<Ponto>& P, Ponto& cp1, Ponto& cp2) {
+double buscarForcaBruta(const std::vector<Ponto>& P, Ponto& cp1, Ponto& cp2) {
     return forcaBrutaLocal(P, 0, P.size(), cp1, cp2);
 }
 
 // varredura linear na faixa de fronteira entre as duas sub-regiões
-double processarFaixaFronteira(vector<Ponto>& faixa, double d_min, Ponto& cp1, Ponto& cp2) {
+double processarFaixaFronteira(std::vector<Ponto>& faixa, double d_min, Ponto& cp1, Ponto& cp2) {
     double min_dist = d_min;
 
     // Ordena os sensores da faixa verticalmente pelo eixo Y 
-    sort(faixa.begin(), faixa.end(), compararY);
+    std::sort(faixa.begin(), faixa.end(), compararY);
 
     // loop limitado a no máximo 7 comparações por elemento 
     for (size_t i = 0; i < faixa.size(); ++i) {
         for (size_t j = i + 1; j < faixa.size() && (faixa[j].y - faixa[i].y) < min_dist; ++j) {
+            if (std::abs(faixa[i].z - faixa[j].z) >= min_dist) continue;
+
             double d = calcularDistancia(faixa[i], faixa[j]);
             if (d < min_dist) {
                 min_dist = d;
@@ -67,7 +67,7 @@ double processarFaixaFronteira(vector<Ponto>& faixa, double d_min, Ponto& cp1, P
 }
 
 // execucao recursiva do algoritmo principal
-double executarDivisaoEConquista(const vector<Ponto>& Px, int inicio, int fim, Ponto& cp1, Ponto& cp2) {
+double executarDivisaoEConquista(const std::vector<Ponto>& Px, int inicio, int fim, Ponto& cp1, Ponto& cp2) {
     if (fim - inicio <= 3) {
         return forcaBrutaLocal(Px, inicio, fim, cp1, cp2);
     }
@@ -79,7 +79,7 @@ double executarDivisaoEConquista(const vector<Ponto>& Px, int inicio, int fim, P
 
     double dl, dr;
     if (fim - inicio > LIMIAR_THREAD) {
-        auto future_esq = async(launch::async, executarDivisaoEConquista, ref(Px), inicio, meio, ref(cp1_esq), ref(cp2_esq));
+        auto future_esq = std::async(std::launch::async, executarDivisaoEConquista, std::ref(Px), inicio, meio, std::ref(cp1_esq), std::ref(cp2_esq));
         dr = executarDivisaoEConquista(Px, meio + 1, fim, cp1_dir, cp2_dir);
         dl = future_esq.get();
     } else {
@@ -99,19 +99,19 @@ double executarDivisaoEConquista(const vector<Ponto>& Px, int inicio, int fim, P
     }
 
     // varredura da interseccao central
-    vector<Ponto> faixa;
+    std::vector<Ponto> faixa;
     faixa.reserve(fim - inicio + 1); 
     for (int i = inicio; i <= fim; i++) {
-        if (abs(Px[i].x - pontoMedio.x) < d) {
+        if (std::abs(Px[i].x - pontoMedio.x) < d) {
             faixa.push_back(Px[i]);
         }
     }
 
-    return min(d, processarFaixaFronteira(faixa, d, cp1, cp2));
+    return std::min(d, processarFaixaFronteira(faixa, d, cp1, cp2));
 }
 
 // ponto de entrada
-double buscarParMaisProximo(vector<Ponto>& P, Ponto& sensor1, Ponto& sensor2) {
-    sort(P.begin(), P.end(), compararX);
+double buscarParMaisProximo(std::vector<Ponto>& P, Ponto& sensor1, Ponto& sensor2) {
+    std::sort(P.begin(), P.end(), compararX);
     return executarDivisaoEConquista(P, 0, P.size() - 1, sensor1, sensor2);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,40 @@
 #include <iomanip>
 #include "point.h"
 
-using namespace std;
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
     int n;
-    if (!(cin >> n)) return 0;
+    if (!(std::cin >> n)) return 0;
 
-    vector<Ponto> sensores;
+    std::vector<Ponto> sensores;
     sensores.reserve(n);
 
     for (int i = 0; i < n; i++) {
         Ponto p;
-        cin >> p.id >> p.x >> p.y >> p.z;
+        std::cin >> p.id >> p.x >> p.y >> p.z;
         sensores.push_back(p);
     }
 
     Ponto s1_bf, s2_bf;
-    auto t_inicio_bf = chrono::high_resolution_clock::now();
+    auto t_inicio_bf = std::chrono::high_resolution_clock::now();
     double dist_bf = buscarForcaBruta(sensores, s1_bf, s2_bf);
-    auto t_fim_bf = chrono::high_resolution_clock::now();
-    double duracao_bf = chrono::duration<double, milli>(t_fim_bf - t_inicio_bf).count();
+    auto t_fim_bf = std::chrono::high_resolution_clock::now();
+    double duracao_bf = std::chrono::duration<double, std::milli>(t_fim_bf - t_inicio_bf).count();
 
     Ponto s1_dc, s2_dc;
-    auto t_inicio_dc = chrono::high_resolution_clock::now();
+    auto t_inicio_dc = std::chrono::high_resolution_clock::now();
     double dist_dc = buscarParMaisProximo(sensores, s1_dc, s2_dc);
-    auto t_fim_dc = chrono::high_resolution_clock::now();
-    double duracao_dc = chrono::duration<double, milli>(t_fim_dc - t_inicio_dc).count();
+    auto t_fim_dc = std::chrono::high_resolution_clock::now();
+    double duracao_dc = std::chrono::duration<double, std::milli>(t_fim_dc - t_inicio_dc).count();
 
-    cout << fixed << setprecision(4);
-    cout << "MIN_DIST=" << dist_dc << "\n";
-    cout << "P1_ID=" << s1_dc.id << " X=" << s1_dc.x << " Y=" << s1_dc.y << " Z=" << s1_dc.z << "\n";
-    cout << "P2_ID=" << s2_dc.id << " X=" << s2_dc.x << " Y=" << s2_dc.y << " Z=" << s2_dc.z << "\n";
-    cout << "TIME_BF_MS=" << duracao_bf << "\n";
-    cout << "TIME_DC_MS=" << duracao_dc << "\n";
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << "MIN_DIST=" << dist_dc << "\n";
+    std::cout << "P1_ID=" << s1_dc.id << " X=" << s1_dc.x << " Y=" << s1_dc.y << " Z=" << s1_dc.z << "\n";
+    std::cout << "P2_ID=" << s2_dc.id << " X=" << s2_dc.x << " Y=" << s2_dc.y << " Z=" << s2_dc.z << "\n";
+    std::cout << "TIME_BF_MS=" << duracao_bf << "\n";
+    std::cout << "TIME_DC_MS=" << duracao_dc << "\n";
 
     return 0;
 }
